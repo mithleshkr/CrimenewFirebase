0d8954f09eb692c06665893e07d6970b3e0f4cf4
mithleshkr <79351404+mithleshkr@users.noreply.github.com>
2021-12-07 13:02:12 +0530

    Update Dashboard.js

--- Dashboard.js
+++ Dashboard.js
@@ -1,16 +1,31 @@
 import React from 'react'
 import './Dashboard.css'
+import './Sidebar.css'
 import {Search, Person, Chat, Notifications} from '@material-ui/icons'
+import {
+    RssFeed,
+    
+    PlayCircleFilledOutlined,
+    Group,
+    Bookmark,
+    HelpOutline,
+    WorkOutline,
+    Event,
+    School,
+  } from "@material-ui/icons";
+//import Sidebar from './Sidebar'
+
 
 function Dashboard() {
     return (
+        <div>
         <div className="topbarContainer">
             <div className="topbarLeft">
                 <span className="logo">CrimeAlert</span>
             </div>
                 <div className="topbarCenter">
                     <div className="searchbar">
-                        <Search />
+                        <Search className="searchIcon" />
                         <input placeholder="Search for post" className="searchInput" />
                     </div>
                 </div>
@@ -32,15 +47,62 @@ function Dashboard() {
                             <Notifications />
                             <span className="topbarIconBadge">3</span>
                         </div>
+                        
                     </div>
+                    
+                    
 
-                </div>
-
-                
-
+                </div>  
             
-            
+        </div> 
+        {/* Sidebar code */}
+        <div className="sidebar">
+        <div className="sidebarWrapper">
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <RssFeed className="sidebarIcon" />
+              <span className="sidebarListItemText">Feed</span>
+            </li>
+            <li className="sidebarListItem">
+              <Chat className="sidebarIcon" />
+              <span className="sidebarListItemText">Chats</span>
+            </li>
+            <li className="sidebarListItem">
+              <PlayCircleFilledOutlined className="sidebarIcon" />
+              <span className="sidebarListItemText">Videos</span>
+            </li>
+            <li className="sidebarListItem">
+              <Group className="sidebarIcon" />
+              <span className="sidebarListItemText">Groups</span>
+            </li>
+            <li className="sidebarListItem">
+              <Bookmark className="sidebarIcon" />
+              <span className="sidebarListItemText">Bookmarks</span>
+            </li>
+            <li className="sidebarListItem">
+              <HelpOutline className="sidebarIcon" />
+              <span className="sidebarListItemText">Questions</span>
+            </li>
+            <li className="sidebarListItem">
+              <WorkOutline className="sidebarIcon" />
+              <span className="sidebarListItemText">Jobs</span>
+            </li>
+            <li className="sidebarListItem">
+              <Event className="sidebarIcon" />
+              <span className="sidebarListItemText">Events</span>
+            </li>
+            <li className="sidebarListItem">
+              <School className="sidebarIcon" />
+              <span className="sidebarListItemText">Courses</span>
+            </li>
+          </ul>
+          {/* <button className="sidebarButton">Show More</button> */}
+          {/* <hr className="sidebarHr" /> */}
+          
         </div>
+      </div>
+        </div>
+        
     )
 }
 
