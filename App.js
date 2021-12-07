f0dd7e74d65871b64637fd9718cbd8b0e8ec5c80
mithleshkr <79351404+mithleshkr@users.noreply.github.com>
2021-12-07 11:58:01 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import SignInOutContainer from './combine'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './screens/Dashboard';
 
 
 
 function App() {
 	return (
 		<div>
-			<SignInOutContainer />
+			<Router>
+				<Routes>
+					<Route exact path='/' element={<SignInOutContainer />} />
+					<Route path='/home' element={<Dashboard />} />
+				</Routes>
+			</Router>
 		</div>
 	)
 }
